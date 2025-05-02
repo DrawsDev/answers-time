f9842f3edcdc5198a42cfc12cd5b8bac4881d0b8
drawsdev <andrew04boom@gmail.com>
2025-05-02 15:31:54 +0500

    add error.log for compiled applications

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import os
+import sys
 from src.core.game import Game
 from content.scenes.menu import Menu
 from content.scenes.intro import Intro
 
 if __name__ == "__main__":
+    if hasattr(sys, "_MEIPASS"):
+        sys.stderr = open(os.path.join(os.getcwd(), "error.log"), "w")
+    
     game = Game()
     game.add_scene("Menu", Menu)
     game.add_scene("Intro", Intro)
