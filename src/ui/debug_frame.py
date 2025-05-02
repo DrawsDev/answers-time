86223058051840561c3fbc973bacc304461029c3
drawsdev <andrew04boom@gmail.com>
2025-05-02 15:53:59 +0500

    update font path

--- src/ui/debug_frame.py
+++ src/ui/debug_frame.py
@@ -1,3 +1,4 @@
+import os
 import pygame
 from src.settings import *
 from src.core.game import Game
@@ -5,7 +6,7 @@ from src.core.utility import path
 
 class DebugFrame:
     def __init__(self, game: Game):
-        self.font = pygame.font.Font(path(f"{FONTS}Tiny5-Regular.ttf"), 8)
+        self.font = pygame.font.Font(path(os.path.join(FONTS, "Tiny5-Regular.ttf")), 8)
         self.clock = game.clock
     
     def update(self, delta: float) -> None:
