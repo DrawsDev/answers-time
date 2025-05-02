d6a464d1daa934c15a57303dbb9cd23c84e5ec29
drawsdev <andrew04boom@gmail.com>
2025-05-02 15:37:43 +0500

    refactor path function

--- src/core/utility.py
+++ src/core/utility.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 def clamp(v: float, min_v: float, max_v: float) -> float:
@@ -7,6 +8,9 @@ def lerp(a: float, b: float, t: float) -> float:
     return a + (b - a) * t
 
 def path(relative_path: str) -> str:
-    if hasattr(sys, "_MEIPASS"):
-        return f"{sys._MEIPASS}\{relative_path}" 
-    return relative_path
+    """Получение абсолютного пути до файла."""
+    base_path = getattr(sys, "_MEIPASS", os.getcwd())
+    # Если файл не запакован в exe (на деле костыль :P)
+    if not os.path.exists(base_path):
+        base_path = os.getcwd()
+    return os.path.join(base_path, relative_path)
