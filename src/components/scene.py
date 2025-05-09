fe3e0dccb4fa3e742f1f9c34885e81a3c51c691f
drawsdev <andrew04boom@gmail.com>
2025-05-09 14:56:14 +0500

    remove sprites variable

--- src/components/scene.py
+++ src/components/scene.py
@@ -1,15 +1,8 @@
 import pygame
 
 class Scene:
-    def __init__(self) -> None:
-        self.sprites = pygame.sprite.Group()
-    
+    def __init__(self) -> None: ...
     def on_exit(self, **kwargs) -> None: ...
-
     def on_enter(self, **kwargs) -> None: ...
-
-    def update(self, delta: float) -> None:
-        self.sprites.update(delta)
-
-    def draw(self, surface: pygame.Surface) -> None:
-        self.sprites.draw(surface)
+    def update(self, delta: float) -> None: ...
+    def draw(self, surface: pygame.Surface) -> None: ...
