08fd88d5fb140761569feac7ca7fa96177bcffec
drawsdev <andrew04boom@gmail.com>
2025-05-10 23:59:27 +0500

    add Input class

---
+++ src/core/input.py
@@ -0,0 +1,68 @@
+import pygame
+
+MOUSE_KEY_ALIASES = ("m_none", "m_left", "m_middle", "m_right", "m_wheel_up", "m_wheel_down", "m_extra_1", "m_extra_2")
+
+class Input:
+    def __init__(self):
+        self._released_keys = set()
+        self._pressed_keys = set()
+        self._down_keys = set()
+        self._mouse_moved = False
+        self._unicode = ""
+    
+    def is_mouse_moved(self) -> bool:
+        return self._mouse_moved
+
+    def get_unicode(self) -> str:
+        return self._unicode
+
+    def get_axis(self, positive_key: str, negative_key: str) -> int:
+        key1 = int(self.is_key_down(positive_key))
+        key2 = int(self.is_key_down(negative_key))
+        return key1 - key2
+    
+    def is_key_pressed(self, key: str) -> bool:
+        return key in self._pressed_keys
+
+    def is_key_released(self, key: str) -> bool:
+        return key in self._released_keys
+
+    def is_key_down(self, key: str) -> bool:
+        return key in self._down_keys
+
+    def is_anything_pressed(self) -> bool:
+        return len(self._pressed_keys) > 0
+
+    def update(self) -> None:
+        self._pressed_keys.clear()
+        self._released_keys.clear()
+        self._unicode = ""
+
+    def handle_event(self, event: pygame.Event) -> None:
+        if event.type == pygame.KEYUP:
+            key_name = pygame.key.name(event.key)
+            self._down_keys.discard(key_name)
+            self._released_keys.add(key_name)
+
+        if event.type == pygame.KEYDOWN:
+            key_name = pygame.key.name(event.key)
+            self._down_keys.add(key_name)
+            self._pressed_keys.add(key_name)
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            key_name = MOUSE_KEY_ALIASES[event.button]
+            self._down_keys.discard(key_name)
+            self._released_keys.add(key_name)
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            key_name = MOUSE_KEY_ALIASES[event.button]
+            self._down_keys.add(key_name)
+            self._pressed_keys.add(key_name)
+
+        if event.type == pygame.TEXTINPUT:
+            self._unicode = event.text
+
+        if self.is_key_pressed("f11"):
+            pygame.display.toggle_fullscreen()
+
+        self._mouse_moved = event.type == pygame.MOUSEMOTION
