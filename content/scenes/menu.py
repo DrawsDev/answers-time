72f49355b907631ecf223b00328fdb57a8ca4aad
drawsdev <andrew04boom@gmail.com>
2025-05-16 23:55:57 +0500

    add test label

--- content/scenes/menu.py
+++ content/scenes/menu.py
@@ -6,9 +6,10 @@ from src.settings import *
 from src.core.game import Game
 from src.core.utility import path
 from src.components.scene import Scene
-from src.components.sprite import *
+from src.components.sprite import Sprite
 from src.ui.debug_frame import DebugFrame
 from src.ui.base.button import UIButton
+from src.ui.label import Label
 
 class Menu(Scene):
     def __init__(self, game: Game):
@@ -17,7 +18,7 @@ class Menu(Scene):
         self.ui_objects = pygame.sprite.Group()
         self.debug_frame = DebugFrame(game)
 
-        image = pygame.image.load(path(os.path.join(SPRITES, "logo.png")))
+        image = pygame.image.load(path(os.path.join(SPRITES, "new_logo.png")))
         image.set_colorkey("Black")
 
         self.sprite = Sprite(image, (game.surface.get_width() / 2, game.surface.get_height() / 2), Anchor.Center)
@@ -27,6 +28,10 @@ class Menu(Scene):
         self.button.anchor = Anchor.Center
         self.ui_objects.add(self.button)
 
+        self.label = Label(game, "Hello World!", [game.surface.get_width() / 2, 50])
+        self.label.anchor = Anchor.Center
+        self.ui_objects.add(self.label)
+
     def update(self, delta: float):
         self.debug_frame.update(delta)
         self.ui_objects.update(delta)
@@ -34,6 +39,12 @@ class Menu(Scene):
 
         if self.button.pressed:
             self.sprite.anchor = random.choice(list(Anchor))
+            self.label.text = str(self.sprite.anchor.name)
+            self.label.color = random.choice(list(pygame.color.THECOLORS.keys()))
+            self.label.background_color = random.choice(["BLACK", "DARKGRAY", "WHITE", "BROWN"])
+            self.label.fontpath = random.choice([path(os.path.join(FONTS, "Ramona-Bold.ttf")), path(os.path.join(FONTS, "m5x7.ttf")), None])
+            self.label.fontsize = random.choice([30, 8, 20])
+            self.label.background_padding = random.randint(5, 20)
 
     def draw(self, surface: pygame.Surface):
         surface.fill("Gray")
