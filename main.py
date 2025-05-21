3bdb35db99c38ce8c331b4f84f2f93319effc330
drawsdev <andrew04boom@gmail.com>
2025-05-21 23:58:52 +0500

    add Tutorial scene

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from src.core.game import Game
 from content.scenes.menu import Menu
 from content.scenes.intro import Intro
+from content.scenes.tutorial import Tutorial
 
 if __name__ == "__main__":
     if hasattr(sys, "_MEIPASS"):
@@ -11,5 +12,6 @@ if __name__ == "__main__":
     game = Game()
     game.add_scene("Menu", Menu)
     game.add_scene("Intro", Intro)
+    game.add_scene("Tutorial", Tutorial)
     game.change_scene("Menu")
     game.run()
