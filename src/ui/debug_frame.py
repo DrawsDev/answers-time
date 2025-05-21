1b69b9bd3bdada358096a88dc5b1282bc819abe0
drawsdev <andrew04boom@gmail.com>
2025-05-21 23:09:22 +0500

    add new debug info

--- src/ui/debug_frame.py
+++ src/ui/debug_frame.py
@@ -1,19 +1,33 @@
 import os
 import pygame
-from src.settings import *
+from src.settings import FONTS, SURFACE_SIZE
 from src.core.game import Game
 from src.core.utility import path
 
 class DebugFrame:
     def __init__(self, game: Game):
         self.font = pygame.font.Font(path(os.path.join(FONTS, "Tiny5-Regular.ttf")), 8)
+        self.charsize = self.font.size(" ")
         self.clock = game.clock
     
+    def _render(self, text: str) -> pygame.Surface:
+        return self.font.render(text, True, "WHITE")
+
     def update(self, delta: float) -> None:
         pass
 
     def draw(self, surface: pygame.Surface) -> None:
-        fps = self.clock.get_fps()
-        fps_counter = self.font.render("FPS: %.1f" % fps, True, "White")
-
-        surface.blit(fps_counter, (10, 10))
+        # VER
+        ver = self._render("VER: 1.0")
+        surface.blit(ver, [0, 0])
+        # FPS
+        fps = self._render("FPS: %.1f" % self.clock.get_fps())
+        surface.blit(fps, [0, self.charsize[1]])
+        # MOUSE POSITION
+        mouse_pos = pygame.mouse.get_pos()
+        window_size = pygame.display.get_window_size()
+        ratio = tuple([window_size[0] / SURFACE_SIZE[0], window_size[1] / SURFACE_SIZE[1]])
+        ratio_mouse_pos = tuple([mouse_pos[0] / ratio[0], mouse_pos[1] / ratio[1]])
+        surface.blit(self._render("WIN_POS: " + str(mouse_pos)), [0, self.charsize[1] * 2])
+        surface.blit(self._render("SRF_POS: " + str(ratio_mouse_pos)), [0, self.charsize[1] * 3])
+        surface.blit(self._render("RATIO: " + str(ratio)), [0, self.charsize[1] * 4])
