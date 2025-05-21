28925e7207365f53f48f4b579b87c1df48884f77
drawsdev <andrew04boom@gmail.com>
2025-05-21 23:56:56 +0500

    add Frame class

---
+++ src/ui/frame.py
@@ -0,0 +1,53 @@
+from pygame import Surface, Color
+from typing import Tuple, Optional
+from src.core.game import Game
+from src.core.utility import clamp
+from src.ui.base.ui_object import UIObject
+
+class Frame(UIObject):
+    def __init__(self, 
+                 game: Game, 
+                 size: Tuple[int, int] = [100, 100], 
+                 position: Tuple[int, int] = [0, 0]
+                 ):
+        super().__init__(game, size, position)
+
+        self._size = size
+        self._alpha = 255
+        self._color = "BLACK"
+        self._update_image()
+
+    @property
+    def color(self) -> Color:
+        return self._color
+
+    @property
+    def transparency(self) -> Optional[int]:
+        return self._alpha
+    
+    @property
+    def size(self) -> Tuple[int, int]:
+        return self._size
+
+    @color.setter
+    def color(self, value: Color) -> None:
+        self._color = value
+        self._update_image()
+
+    @transparency.setter
+    def transparency(self, value: int) -> None:
+        self._alpha = clamp(value, 0, 255)
+        self._update_image()
+
+    @size.setter
+    def size(self, value: Tuple[int, int]) -> None:
+        self._size = value
+        self._update_image()
+
+    def _update_image(self) -> None:
+        self.image = Surface(self._size)
+        self.image.fill(self._color)
+        self.image.set_alpha(self._alpha)
+        self._update_rect()
+
+__all__ = ["Frame"]
