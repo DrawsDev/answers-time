67e67b1effe36d88568af50fca17a0341bba0558
drawsdev <andrew04boom@gmail.com>
2025-05-21 23:58:13 +0500

    add TextButton class

---
+++ src/ui/text_button.py
@@ -0,0 +1,61 @@
+import pygame
+from typing import Tuple
+from src.core.game import Game
+from src.ui.base.ui_button import UIButton
+
+class TextButton(UIButton):
+    def __init__(self, 
+                 game: Game, 
+                 text: str = "TextButton",
+                 size: Tuple[int, int] = [100, 50],
+                 position: Tuple[int, int] = [0, 0]):
+        self._text = text
+        self._font_wraplength = 200
+        self._font_antialias = True
+        self._font_align = pygame.FONT_LEFT
+        self._font_color = "WHITE"
+        self._font_path = None
+        self._font_size = 20
+        self._font = pygame.Font(self._font_path, self._font_size)
+        super().__init__(game, size, position)
+
+    @property
+    def text(self) -> str:
+        return self._text
+
+    @property
+    def font(self) -> pygame.Font:
+        return self._font
+
+    @property
+    def text_align(self) -> int:
+        return self._font_align
+
+    @property
+    def text_wraplength(self) -> int:
+        return self._font_wraplength
+
+    @text.setter
+    def text(self, value: str) -> None:
+        self._text = value
+        self._update_image()
+
+    @text_align.setter
+    def text_align(self, value: int) -> None:
+        self._font_align = value if value in [0, 1, 2] else 0
+        self._font.align = self._font_align
+        self._update_image()
+
+    @text_wraplength.setter
+    def text_wraplength(self, value: int) -> None:
+        self._font_wraplength = value if value > 0 else 0
+        self._update_image()
+
+    def _update_image(self) -> None:
+        super()._update_image()
+        text_surface = self._font.render(self._text, self._font_antialias, self._font_color, None, self._font_wraplength)
+        text_rect = text_surface.get_rect(**{"center": [self._button_size[0] / 2, self._button_size[1] / 2]})
+        self.image.blit(text_surface, text_rect)
+        self._update_rect()
+
+__all__ = ["TextButton"]
