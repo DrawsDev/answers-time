40407d17474d359c9b467a7a35b377ffbcb4c375
drawsdev <andrew04boom@gmail.com>
2025-05-22 20:32:48 +0500

    update _load_image method, image_path property; rename _path to _image_path

--- src/ui/base/ui_image.py
+++ src/ui/base/ui_image.py
@@ -1,31 +1,31 @@
 import pygame
 from typing import Tuple
 from src.core.game import Game
-from src.core.utility import path
 from src.ui.base.ui_object import UIObject
 
 class UIImage(UIObject):
     def __init__(self, 
                  game: Game,
-                 relative_path: str,
+                 path: str,
                  position: Tuple[int, int] = [0, 0]
                  ):
         super().__init__(game, position=position)
 
-        self._path = relative_path
-        self._load_image(relative_path)
+        self._image_path = path
+        self._load_image()
 
     @property
     def image_path(self) -> str:
-        return self._path
+        return self._image_path
 
     @image_path.setter
-    def image_path(self, relative_path: str) -> None:
-        self._load_image(relative_path)
+    def image_path(self, value: str) -> None:
+        self._image_path = value
+        self._load_image()
 
-    def _load_image(self, relative_path: str) -> None:
-        self.image = pygame.image.load(path(relative_path))
-        self.image.set_colorkey("BLACK")
+    def _load_image(self) -> None:
+        self.image = pygame.image.load(self._image_path)
+        self.image.set_colorkey("Black")
         self._update_rect()
 
 __all__ = ["UIImage"]
