8a1611f0fa6ac312c1b93ca855c7e2666f3e1361
drawsdev <andrew04boom@gmail.com>
2025-05-22 18:09:11 +0500

    add MenuPage class

---
+++ src/ui/menu/menu_page.py
@@ -0,0 +1,33 @@
+from typing import Tuple
+from src.enums import Anchor, TextAlign
+from src.core.game import Game
+from src.ui.label import Label
+from src.ui.frame import Frame
+
+ATBLUE = "#3CA4FF"
+
+class MenuPage:
+    def __init__(self, game: Game, title: str):
+        self.game = game
+        self._create(title)
+
+    @property
+    def children(self) -> Tuple[Frame, Label, Frame]:
+        return tuple([self.frame, self.title, self.line])
+
+    def _create(self, title: str) -> None:
+        # Фрейм
+        self.frame = Frame(self.game, self.game.surface.get_size())
+        self.frame.transparency = 225
+        # Заголовок
+        self.title = Label(self.game, title, [self.game.surface.get_width() / 2, 0])
+        self.title.align = TextAlign.Center
+        self.title.anchor = Anchor.MidTop
+        self.title.color = ATBLUE
+        # Линия под заголовком
+        self.line = Frame(self.game, [self.game.surface.get_width() - 10, 2])
+        self.line.color = ATBLUE
+        self.line.anchor = Anchor.Center
+        self.line.position = [self.game.surface.get_width() / 2, self.title.image.get_height()]
+
+__all__ = ["Menu"]
