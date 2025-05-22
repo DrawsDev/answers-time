95f055b48dced66aedbe5f686a8a2aae6b623a0e
drawsdev <andrew04boom@gmail.com>
2025-05-22 20:37:27 +0500

    add new text properties

--- src/ui/text_button.py
+++ src/ui/text_button.py
@@ -1,6 +1,6 @@
 import pygame
-from typing import Tuple
-from src.enums import TextAlign
+from typing import Tuple, Optional
+from src.enums import Anchor, TextAlign
 from src.core.game import Game
 from src.ui.base.ui_button import UIButton
 
@@ -8,16 +8,17 @@ class TextButton(UIButton):
     def __init__(self, 
                  game: Game, 
                  text: str = "TextButton",
-                 size: Tuple[int, int] = [100, 50],
-                 position: Tuple[int, int] = [0, 0]):
+                 size: Tuple[int, int] = (100, 50),
+                 position: Tuple[int, int] = (0, 0)):
         self._text = text
-        self._font_wraplength = 200
-        self._font_antialias = True
-        self._font_align = TextAlign.Left
-        self._font_color = "WHITE"
-        self._font_path = None
-        self._font_size = 20
-        self._font = pygame.Font(self._font_path, self._font_size)
+        self._text_anchor = Anchor.Center
+        self._text_align = TextAlign.Center
+        self._text_wraplength = 200
+        self._text_color = "White"
+        self._text_antialias = True
+        self._text_fontpath = None
+        self._text_fontsize = 20
+        self._font = pygame.Font(self._text_fontpath, self._text_fontsize)
         super().__init__(game, size, position)
 
     @property
@@ -25,37 +26,86 @@ class TextButton(UIButton):
         return self._text
 
     @property
-    def font(self) -> pygame.Font:
-        return self._font
+    def text_anchor(self) -> Anchor:
+        return self._text_anchor
 
     @property
     def text_align(self) -> TextAlign:
-        return self._font_align
+        return self._text_align
 
     @property
     def text_wraplength(self) -> int:
-        return self._font_wraplength
+        return self._text_wraplength
+
+    @property
+    def text_color(self) -> pygame.Color:
+        return self._text_color
+
+    @property
+    def text_antialias(self) -> bool:
+        return self._text_antialias
+
+    @property
+    def text_fontpath(self) -> str:
+        return self._text_fontpath
+
+    @property
+    def text_fontsize(self) -> int:
+        return self._text_fontsize
+
+    @property
+    def font(self) -> pygame.Font:
+        return self._font
 
     @text.setter
     def text(self, value: str) -> None:
         self._text = value
         self._update_image()
 
+    @text_anchor.setter
+    def text_anchor(self, value: Anchor) -> None:
+        self._text_anchor = value
+        self._update_image()
+
     @text_align.setter
     def text_align(self, value: TextAlign) -> None:
-        self._font_align = value
-        self._font.align = self._font_align
+        self._text_align = value
+        self._font.align = value
         self._update_image()
 
     @text_wraplength.setter
     def text_wraplength(self, value: int) -> None:
-        self._font_wraplength = value if value > 0 else 0
+        if value >= 0:
+            self._text_wraplength = value
+            self._update_image()
+
+    @text_color.setter
+    def text_color(self, value: pygame.Color) -> None:
+        self._text_color = value
         self._update_image()
+
+    @text_antialias.setter
+    def text_antialias(self, value: bool) -> None:
+        self._text_antialias = value
+        self._update_image()
+
+    @text_fontpath.setter
+    def text_fontpath(self, value: Optional[str]) -> None:
+        self._text_fontpath = value
+        self._font = pygame.Font(value, self._text_fontsize)
+        self._update_image()
+
+    @text_fontsize.setter
+    def text_fontsize(self, value: int) -> None:
+        if value >= 0:
+            self._text_fontsize = value
+            self._font = pygame.Font(self._text_fontpath, value)
+            self._update_image()
 
     def _update_image(self) -> None:
         super()._update_image()
-        text_surface = self._font.render(self._text, self._font_antialias, self._font_color, None, self._font_wraplength)
-        text_rect = text_surface.get_rect(**{"center": [self._button_size[0] / 2, self._button_size[1] / 2]})
+        text_surface = self._font.render(self._text, self._text_antialias, self._text_color, None, self._text_wraplength)
+        text_rect = text_surface.get_rect(**{self._text_anchor: [self._button_size[0] / 2, self._button_size[1] / 2]})
         self.image.blit(text_surface, text_rect)
         self._update_rect()
 
