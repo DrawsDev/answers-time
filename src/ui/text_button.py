0675d531d498d624eba8d6ae346c3301c4f2433f
drawsdev <andrew04boom@gmail.com>
2025-05-27 14:12:47 +0500

    add new functionality of an automatically scalable font (WIP)

--- src/ui/text_button.py
+++ src/ui/text_button.py
@@ -3,6 +3,7 @@ import pygame
 from typing import Tuple, Optional
 from src.enums import Align
 from src.core.game import Game
+from src.core.utility import wrap_text
 from src.ui.base.ui_button import UIButton
 
 class TextButton(UIButton):
@@ -12,12 +13,13 @@ class TextButton(UIButton):
         text: str = "TextButton",
         size: Tuple[int, int] = (100, 50),
         position: Tuple[int, int] = (0, 0)
-    ):
+    ) -> None:
         self._button_icon = None
         self._text = text
         self._text_color = "White"
         self._font = pygame.Font()
         self._font_path = None
+        self._font.align = Align.Center
         super().__init__(game, size, position)
 
     @property
@@ -96,10 +98,35 @@ class TextButton(UIButton):
             icon_rect = self._button_icon.get_rect(midleft=(5, self._size[1] / 2))
             self.image.blit(self._button_icon, icon_rect)
 
-        text_surface = self._font.render(self._text, True, self._text_color, None, self._size[0] - offset)
-        text_x = offset if self._font.align == Align.Left else (self._size[0] - text_surface.get_width() + offset) / 2
-        text_rect = text_surface.get_rect(midleft=(text_x, self._size[1] / 2))
-        
-        self.image.blit(text_surface, text_rect)
+        # Автоматический подбор размера шрифта, разбиение текста по линиям и отрисовка
+        if self._font.size(self._text)[0] > self._size[0]:
+            font_size, lines = wrap_text(self._text, self._font_path, self._size[0] - offset, self._size[1], self._font.get_point_size())
+            font = pygame.Font(self._font_path, font_size)
+            font.align = self._font.align
+
+            y_offset = 0
+            line_height = font.get_linesize()
+
+            for index, line in enumerate(lines):
+                text_surface = font.render(line, True, self._text_color, None)
+                
+                if self._font.align == Align.Left:
+                    text_x = offset
+                elif self._font.align == Align.Center:
+                    text_x = (self._size[0] - text_surface.get_width() + offset) / 2
+                elif self._font.align == Align.Right:
+                    text_x = self._size[0] - text_surface.get_width()
+                
+                text_y = (self._size[1] + y_offset - line_height * (len(lines) - 1 - index)) / 2
+                text_rect = text_surface.get_rect(midleft=(text_x, text_y))
+                self.image.blit(text_surface, text_rect)
+                y_offset += line_height
+        # Стандартная отрисовка одной линии
+        else:
+            text_surface = self._font.render(self._text, True, self._text_color, None, self._size[0] - offset)
+            text_x = offset if self._font.align == Align.Left else (self._size[0] - text_surface.get_width() + offset) / 2
+            text_y = self._size[1] / 2
+            text_rect = text_surface.get_rect(midleft=(text_x, text_y))
+            self.image.blit(text_surface, text_rect)
 
 __all__ = ["TextButton"]
