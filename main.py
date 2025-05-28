30d8a18be6f986d3d9f0b4e9c5eb9d7afedf2d3b
drawsdev <andrew04boom@gmail.com>
2025-05-28 13:48:06 +0500

    add Quiz and Editor scenes

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import sys
 from src.core.game import Game
 from content.scenes.menu import Menu
-from content.scenes.intro import Intro
+from content.scenes.quiz import Quiz
+from content.scenes.editor import Editor
 from content.scenes.tutorial import Tutorial
 
 if __name__ == "__main__":
@@ -11,7 +12,8 @@ if __name__ == "__main__":
     
     game = Game()
     game.add_scene("Menu", Menu)
-    game.add_scene("Intro", Intro)
+    game.add_scene("Quiz", Quiz)
+    game.add_scene("Editor", Editor)
     game.add_scene("Tutorial", Tutorial)
     game.change_scene("Menu")
     game.run()
