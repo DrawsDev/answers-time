1e79f2466aa2f156deebede4e8bf3eddd32e9bc5
drawsdev <andrew04boom@gmail.com>
2025-05-29 00:27:13 +0500

    change type of arguments

--- src/ui/menu/menu_page.py
+++ src/ui/menu/menu_page.py
@@ -22,15 +22,15 @@ class MenuPage:
         self.frame = Frame(self.game, self.game.surface.get_size())
         self.frame.transparency = 225
         # Заголовок
-        self.title = TextLabel(self.game, title, [self.game.surface.get_width() / 2, 0])
+        self.title = TextLabel(self.game, title, (self.game.surface.get_width() / 2, 0))
         self.title.anchor = Anchor.MidTop
         self.title.text_color = ATBLUE
         self.title.font_path = asset_path(FONTS, "Ramona-Bold.ttf")
         self.title.font_size = 16
         # Линия под заголовком
-        self.line = Frame(self.game, [self.game.surface.get_width() - 10, 2])
+        self.line = Frame(self.game, (self.game.surface.get_width() - 10, 2))
         self.line.color = ATBLUE
         self.line.anchor = Anchor.Center
-        self.line.position = [self.game.surface.get_width() / 2, self.title.image.get_height()]
+        self.line.position = (self.game.surface.get_width() / 2, self.title.size[1])
 
 __all__ = ["Menu"]
