208b1527d3967124e22b38ecd7bac1e31e3ef966
drawsdev <andrew04boom@gmail.com>
2025-05-31 01:39:01 +0500

    refactor __init__, properties and methods

--- src/ui/frame.py
+++ src/ui/frame.py
@@ -1,43 +1,34 @@
-from pygame import Surface, Color
-from typing import Tuple, Optional
+import pygame
+from typing import Tuple
+from src.enums import Anchor
 from src.core.game import Game
-from src.core.utility import clamp
 from src.ui.base.ui_object import UIObject
 
 class Frame(UIObject):
-    def __init__(self, 
-                 game: Game, 
-                 size: Tuple[int, int] = (100, 100), 
-                 position: Tuple[int, int] = (0, 0)
-                 ):
-        super().__init__(game, size, position)
-
-        self._alpha = 255
-        self._color = "BLACK"
+    def __init__(
+        self,
+        game: Game,
+        color: pygame.Color = "black",
+        size: Tuple[int, int] = (100, 100), 
+        position: Tuple[int, int] = (0, 0),
+        anchor: Anchor = Anchor.TopLeft
+    ) -> None:
+        super().__init__(game, size, position, anchor)
+        self._color = color
         self._update_image()
 
     @property
-    def color(self) -> Color:
+    def color(self) -> pygame.Color:
         return self._color
 
-    @property
-    def transparency(self) -> Optional[int]:
-        return self._alpha
-
     @color.setter
-    def color(self, value: Color) -> None:
-        self._color = value
-        self._update_image()
-
-    @transparency.setter
-    def transparency(self, value: int) -> None:
-        self._alpha = clamp(value, 0, 255)
-        self._update_image()
+    def color(self, value: pygame.Color) -> None:
+        if self._color != value:
+            self._color = value
+            self._update_image()
 
     def _update_image(self) -> None:
-        self.image = Surface(self._size)
+        super()._update_image()
         self.image.fill(self._color)
-        self.image.set_alpha(self._alpha)
-        self._update_rect()
 
 __all__ = ["Frame"]
