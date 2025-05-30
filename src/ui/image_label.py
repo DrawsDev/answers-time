243d34f42b1b1858621ac271e639a72e6d26f190
drawsdev <andrew04boom@gmail.com>
2025-05-31 01:40:26 +0500

    update __init__

--- src/ui/image_label.py
+++ src/ui/image_label.py
@@ -1,13 +1,16 @@
 from typing import Tuple
+from src.enums import Anchor
 from src.core.game import Game
 from src.ui.base.ui_image import UIImage
 
 class ImageLabel(UIImage):
-    def __init__(self, 
-                 game: Game, 
-                 path: str, 
-                 position: Tuple[int, int] = [0, 0]
-                 ):
-        super().__init__(game, path, position)
+    def __init__(
+        self, 
+        game: Game, 
+        path: str, 
+        position: Tuple[int, int] = (0, 0),
+        anchor: Anchor = Anchor.TopLeft
+    ) -> None:
+        super().__init__(game, path, position, anchor)
 
 __all__ = ["ImageLabel"]
