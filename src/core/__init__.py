e0d2f078401c24bb96ac7a3378a80f2f68f75a39
drawsdev <andrew04boom@gmail.com>
2026-02-16 11:33:24 +0500

    add common classes

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,3 +1,4 @@
+from .common import *
 from .application import Application
 from .keyboard import Key, ModifierKey
 from .scene import Scene
---
+++ src/core/common/__init__.py
@@ -0,0 +1,1 @@
+from pygame import Color, Vector2, Vector3
