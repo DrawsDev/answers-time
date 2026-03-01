753e29da94bc45969371c47f1ee158e208e7800f
drawsdev <andrew04boom@gmail.com>
2026-03-01 22:25:12 +0500

    update superclass in Label

--- src/core/objects/ui/label.py
+++ src/core/objects/ui/label.py
@@ -1,26 +1,17 @@
 import pygame
 import pygame.freetype
 
-from src.core.graphics import Drawable
+from src.core.objects.ui.element_ui import ElementUI
 
-class Label(Drawable):
+
+class Label(ElementUI):
     def __init__(self) -> None:
+        super().__init__()
+        self._offset = pygame.Vector2()
         self._text = ""
         self._font = pygame.freetype.Font(None, 24)
         self._font_color = "white"
-        self._position = pygame.Vector2()
-        self._offset = pygame.Vector2()
-        self._anchor = "topleft"
         self._update_surface()
-
-    @property
-    def position(self) -> pygame.Vector2:
-        return self._position
-
-    @position.setter
-    def position(self, value: pygame.Vector2):
-        self._position = value
-        self._update_rect()
 
     @property
     def offset(self) -> pygame.Vector2:
@@ -32,21 +23,21 @@ class Label(Drawable):
         self._update_rect()
 
     @property
-    def anchor(self) -> str:
-        return self._anchor
-    
-    @anchor.setter
-    def anchor(self, value: str) -> None:
-        self._anchor = value
-        self._update_rect()
-
-    @property
     def text(self) -> str:
         return self._text
     
     @text.setter
     def text(self, value: str) -> None:
         self._text = value
+        self._update_surface()
+
+    @property
+    def font_color(self) -> str:
+        return self._font_color
+    
+    @font_color.setter
+    def font_color(self, value: str) -> None:
+        self._font_color = value
         self._update_surface()
 
     @property
@@ -67,15 +58,6 @@ class Label(Drawable):
         if value > 0:
             self._font.size = value
             self._update_surface()
-
-    @property
-    def font_color(self) -> str:
-        return self._font_color
-    
-    @font_color.setter
-    def font_color(self, value: str) -> None:
-        self._font_color = value
-        self._update_surface()
 
     def _update_surface(self) -> None:
         self._surface, _ = self._font.render(self._text, self._font_color)
